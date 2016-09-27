a089328ccd632a0cf5d7c3c5ac1e788a9e9ce0d2
Matt Sanders <msanders45@gmail.com>
2016-09-27 16:57:54 -0600

    got the stupid thing to work :D
    yay

--- client/main.js
+++ client/main.js
@@ -1,22 +1,31 @@
 import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+
+import { Tasks } from '../lib/collections/tasks.js';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+Template.body.helpers({
+    tasks(){
+        return Tasks.find({});
+    },
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
+Template.body.events({
+    'submit .new-task'(event) {
+        // Prevent default browser form submit
+        event.preventDefault();
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+        // Get value from form element
+        const target = event.target;
+        const text = target.text.value;
+
+        // Insert a task into the collection
+        Tasks.insert({
+            text,
+            createdAt: new Date(), // current time
+        });
+
+        // Clear form
+        target.text.value = '';
+    }
 });
