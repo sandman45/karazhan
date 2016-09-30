7465dc5d6a4487268cf0dd8e4e7d50f1f0ee02bd
Matt Sanders <msanders45@gmail.com>
2016-09-30 11:14:25 -0600

    private and public tasks.. publishing etc more security

--- client/templates/tasks/task_items.js
+++ client/templates/tasks/task_items.js
@@ -2,11 +2,21 @@
  * Created by mattsanders on 9/27/16.
  */
 
+Template.taskItem.helpers({
+    isOwner(){
+        return this.owner === Meteor.userId();
+    }
+});
+
+
 Template.taskItem.events({
     'click .toggle-checked'(){
         Meteor.call('tasks.setChecked', this._id, !this.checked);
     },
     'click .delete'(){
         Meteor.call('tasks.remove',this._id);
+    },
+    'click .toggle-private'(){
+        Meteor.call('tasks.setPrivate', this._id, !this.private);
     }
 });
